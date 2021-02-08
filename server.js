90a11a0027551bc68319eced53046b8af3067d70
John Herman <hermanjohn2@gmail.com>
2021-02-08 14:44:35 -0500

    Set up server.js. Enabled middleware, mongodb connection, expression session for user data persistance after login, passport initialization, having server listen for requests.

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express'),
+	session = require('express-session'),
+	mongoose = require('mongoose'),
+	MongoStore = require('connect-mongo')(session),
+	compression = require('compression'),
+	bodyParser = require('body-parser'),
+	passport = require('passport'),
+	dbConfig = require('./config/dbConfig'),
+	app = express(),
+	PORT = process.env.PORT || 3001;
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(compression());
+
+// DB connection
+mongoose.connect(dbConfig.DB_URI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true
+})
+	.then(() => console.log('Connected to DB...'))
+	.catch(err => {
+		console.log('Unable to connect to DB. Exiting...', err);
+		process.exit();
+	});
+
+// Express session
+app.use(
+	session({
+		secret: process.env.SessionSecret || 'working mans truth',
+		resave: false,
+		saveUninitialized: true,
+		store: new MongoStore({ mongooseConnection: mongoose.connection })
+	})
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Routes
+
+// Start the API server
+app.listen(PORT, function () {
+	console.log(`🌎  ==> API Server listening on PORT: ${PORT}`);
+});
